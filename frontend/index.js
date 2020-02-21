f507c5df64db42fdf3c70fe3c9371730c8dff6d3
Sebastian Troedsson <sebastiantroedsson@MacBook-Pro-som-tillhor-Sebastian.local>
2020-02-21 17:01:23 +0100

    Refactored the frontend JS

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,8 @@
-const weatherInfo = document.querySelector('#weather-info');
+const locationInfo = document.querySelector('#location-information');
 const searchButton = document.querySelector('#search-button');
 const searchInput = document.querySelector('#search-input');
-const overView = document.querySelector('#overview');
+const weatherInfo = document.querySelector('#weather-information');
+
 /* Get current location of user at start */
 (function getLocationOfUser() {
   if (navigator.geolocation) {
@@ -15,70 +16,63 @@ const overView = document.querySelector('#overview');
   }
 })();
 
-searchButton.addEventListener('click', e => {
-  e.preventDefault();
-
+/* Fetch data function */
+const fetchData = () => {
   const city = searchInput.value;
   fetch(`http://localhost:5050/api/byCity/${city}`)
     .then(res => res.json())
     .then(res => appendData(res));
-});
+};
+
+/* Add eventlistener on input to send it with enter */
+searchInput.addEventListener('keypress', fetchData);
+searchButton.addEventListener('click', fetchData);
+
 
 const appendData = data => {
   if (data.cod === '404') {
-    weatherInfo.innerHTML = 'There was no match';
+    locationInfo.innerHTML = 'There was no match';
+    weatherInfo.innerHTML = '';
   } else {
-    const { description,
-      temperature,
-      feelsLike,
-      temp_min,
-      temp_max,
-      pressure,
-      humidity,
-      cloudiness,
-      windSpeed,
-      sunrise,
-      sunset,
-      country,
-      location,
-      iconRef } = data;
-
-      
-
-      const dateSunrise = new Date(sunrise * 1000);
-      const formattedSunrise = `${dateSunrise.getHours()}:${dateSunrise.getMinutes()}`;
-      const dateSunset = new Date(sunset * 1000);
-      const formattedSunset = `${dateSunset.getHours()}:${dateSunset.getMinutes()}`;
+    const htmlOverview = `${data.location}, ${data.country} ${data.temperature}&#176C`;
     
-      const htmlOverview = `<h2>${location}, ${country}  <img src="http://openweathermap.org/img/w/${iconRef}.png" /><h2>
-                            
-                             <p>${description} ${temperature}&#176C</p>`;
-
-      const htmlData = `<div id="weather-description">
-                      <p>Feels like:</p>
-                      <p>Max temperature:</p>
-                      <p>Minimun temperature:</p>
-                      <p>Sunrise:</p>
-                      <p>Sunset:</p>
-                      <p>Pressure:</p>
-                      <p>Humidity:</p>
-                      <p>Cloudiness:</p>
-                      <p>Wind:</p>
-                    </div>
-                    <div id="weather-data">
-                      <p>${feelsLike}&#176C</p>
-                      <p>${temp_max}&#176C</p>
-                      <p>${temp_min}&#176C</p>
-                      <p>${formattedSunrise}</p>
-                      <p>${formattedSunset}</p>
-                      <p>${pressure}hPa</p>
-                      <p>${humidity}%</p>
-                      <p>${cloudiness}%</p>
-                      <p>${windSpeed}m/s</p>
-                    </div>`;
-    
-      weatherInfo.innerHTML = htmlData;
-      overView.innerHTML = htmlOverview;
+    const htmlData = `
+      <div class="row">
+        <p>Feels like:</p>
+        <p>${data.feelsLike}&#176C</p>
+      </div>
+      <div class="row">
+        <p>Max temp:</p>
+        <p>${data.temp_max}&#176C</p>
+      </div>
+      <div class="row">
+        <p>Min temp:</p>
+        <p>${data.temp_min}&#176C</p>
+      </div>
+      <div class="row">
+        <p>Pressure:</p>
+        <p>${data.pressure} hPa</p>
+      </div>
+      <div class="row">
+        <p>Humidity:</p>
+        <p>${data.humidity} %</p>
+      </div>
+      <div class="row">
+        <p>Cloudiness:</p>
+        <p>${data.cloudiness} %</p>
+      </div>
+      <div class="row">
+        <p>Wind speed:</p>
+        <p>${data.windSpeed} m/s</p>
+      </div>
+    `;
+    locationInfo.innerHTML = htmlOverview;
+    weatherInfo.innerHTML = htmlData;
   }
 };
 
+
+// const dateSunrise = new Date(sunrise * 1000);
+// const formattedSunrise = `${dateSunrise.getHours()}:${dateSunrise.getMinutes()}`;
+// const dateSunset = new Date(sunset * 1000);
+// const formattedSunset = `${dateSunset.getHours()}:${dateSunset.getMinutes()}`;
